27609c3d992401dda04b95ca0a52787358019f80
christusdsouza <46989701+christusdsouza@users.noreply.github.com>
2022-04-23 01:26:28 +0530

    pdfview: basic webpage + functionality

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,49 @@
 let intro = document.getElementById('todays-subject');
-let date = new Date().getDate()
-const tt = { '22': 'SPM', '23': 'IOT', '26': 'AWP', '28': 'AI', '29': 'NGT' }
+let urlbox = document.getElementById("urlbox");
 
-window.addEventListener('load', () => {
-    intro.innerHTML = (tt[date]) ? tt[date] : date;
+window.addEventListener('load', (event) => {
+    intro.innerHTML = new Date().getDate();
+    if (window.location.pathname != "/" && window.location.search == "") {
+        window.location = window.location.host + "?" + window.location.pathname
+    }
+    changeURL(event);
+});
+document.getElementsByTagName("button")[0].addEventListener('click', (event) => {
+    event.preventDefault();
+    changeURL(event.target);
 });
 
+function changeURL(event) {
+    try {
+        let url = "";
+        if (urlbox.value) {
+            url = new URL(urlbox.value);
+        } else {
+            if (window.location.search === "" || event.type === "submit") {
+                document.getElementById("embed")?.remove();
+                document.getElementById("url_err") ?? document.getElementsByTagName("header")[0].appendChild(
+                    Object.assign(document.createElement("span"), {
+                        textContent: "NO URL",
+                        id: "url_err",
+                    })
+                );
+                return window.history.pushState({}, "", window.location.href.split("?")[0]);
+            } else {
+                url = new URL(window.location.search.substring(1) ?? urlbox.value);
+                urlbox.value = url.href;
+            }
+        }
+
+        document.getElementsByTagName("div")[0].append(
+            Object.assign(document.createElement("embed"), {
+                src: url,
+                height: "100%",
+                width: "100%",
+                id: "embed",
+            })
+        );
+        return document.getElementById("url_err")?.remove();
+    } catch (e) {
+        console.error(e);
+    }
+}
